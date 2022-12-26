3a4d5222d5ffd2b4544d75560916b5316f5c88ea
venkatesh <gr.venkatesh270@gmail.com>
2022-12-26 13:04:37 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import Reverify from './components/shared/alerts/Reverify';
+import UnsuccessAlert from './components/shared/alerts/UnSuccess';
+
 
 function App() {
   return (
     <div className="App">
-     <Reverify/>
+      <UnsuccessAlert/>
     </div>
   );
 }
